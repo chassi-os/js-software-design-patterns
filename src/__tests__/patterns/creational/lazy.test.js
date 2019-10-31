e5311eea207f2f12b229dc55e80b96be831158b6
Christopher Dimoulis <christopherjamesd@gmail.com>
2019-10-31 16:01:08 -0500

    test green and build lazy initialize pattern

---
+++ src/__tests__/patterns/creational/lazy.test.js
@@ -0,0 +1,39 @@
+import makeLazy from '../../../patterns/creational/lazy';
+
+class Dog {
+	bark() {}
+	growl() {}
+}
+
+describe('Lazy test', () => {
+	it('should return all methods of passed class', () => {
+		const lazyDog = makeLazy(Dog);
+		expect(lazyDog.bark).toBeDefined();
+		expect(lazyDog.growl).toBeDefined();
+	});
+
+	it('should call constructor once on first fuction call', () => {
+		const mock = jest.fn();
+		mock.prototype.bark = Dog.prototype.bark;
+		mock.prototype.growl = Dog.prototype.growl;
+		const lazyDog = makeLazy(mock);
+		expect(mock.mock.instances).toHaveLength(0);
+		lazyDog.bark();
+		expect(mock.mock.instances).toHaveLength(1);
+		lazyDog.growl();
+		lazyDog.bark();
+		expect(mock.mock.instances).toHaveLength(1);
+	});
+
+	it('should throw error if it is passed an object with prototype in it', () => {
+		let thrown = false;
+		const lazyDog = makeLazy({ prototype: {} });
+		try {
+			lazyDog.bark();
+		} catch (e) {
+			thrown = true;
+		}
+
+		expect(thrown).toBe(true);
+	});
+});
