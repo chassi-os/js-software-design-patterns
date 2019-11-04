29188ce2e4d320763a895fd13df61499e3e8e8b5
Anthony Kulis <anthony@chassi.com>
2019-11-04 08:32:13 -0600

    builds and tests green the command pattern

---
+++ src/__tests__/patterns/behavioral/command.test.js
@@ -0,0 +1,50 @@
+import Commander from '../../../patterns/behavioral/command';
+
+describe('Commander tests', () => {
+	let commander;
+
+	beforeEach(() => {
+		commander = new Commander();
+	});
+
+	it('should regesiter something to be commanded', () => {
+		const fn = jest.fn();
+		commander.register('start', fn);
+		expect(commander.commands.size).toBe(1);
+		expect(commander.commands.get('start').has(fn)).toBe(true);
+	});
+
+	it('should be able to register another execute function under same command', () => {
+		const fn1 = jest.fn();
+		const fn2 = jest.fn();
+		commander.register('start', fn1);
+		commander.register('start', fn2);
+		expect(commander.commands.size).toBe(1);
+		expect(commander.commands.get('start').has(fn1)).toBe(true);
+		expect(commander.commands.get('start').has(fn2)).toBe(true);
+	});
+
+	it('should be able to register mulitple commands', () => {
+		const fn = jest.fn();
+		commander.register('a', fn);
+		commander.register('b', fn);
+		expect(commander.commands.size).toBe(2);
+		expect(commander.commands.get('a').has(fn)).toBe(true);
+		expect(commander.commands.get('b').has(fn)).toBe(true);
+	});
+
+	it('should be able to execute a command', () => {
+		const run = jest.fn();
+		const dontRun = jest.fn();
+		commander.register('a', run);
+		commander.register('b', dontRun);
+		commander.execute('a', 'foo', 'bar');
+		expect(run).toHaveBeenCalledTimes(1);
+		expect(run).toHaveBeenCalledWith('foo', 'bar');
+		expect(dontRun).toHaveBeenCalledTimes(0);
+		const nowRun = dontRun;
+		commander.execute('b');
+		expect(nowRun).toHaveBeenCalledTimes(1);
+		expect(run).toHaveBeenCalledTimes(1);
+	});
+});
