4d2d5e862bc6ce6891e57a21e59607f9c0a5cc10
Anthony Kulis <anthony@chassi.com>
2019-11-04 14:49:45 -0600

    changes paradigm of bridge and reduces it to a simple function call

--- src/patterns/behavioral/bridge.js
+++ src/patterns/behavioral/bridge.js
@@ -1,22 +1,5 @@
-class Bridge {
-	functionMappings = new Map();
+const bridge = (keyScope, keyFunc) => ({
+	to: (toScope, toFunc) => (...args) => toFunc.apply(toScope, [keyFunc.apply(keyScope, args)]),
+});
 
-	join = (keyFunc, nextFunc) => {
-		this.functionMappings.set(keyFunc.name, nextFunc);
-		const me = this;
-		return (...args) => {
-			const value = keyFunc(args);
-			return me.functionMappings.get(keyFunc.name)(value);
-		};
-	};
-
-	_apply = keyFunc => {
-		const me = this;
-		return (...args) => {
-			const value = keyFunc(args);
-			return me.functionMappings.get(keyFunc.name)(value);
-		};
-	};
-}
-
-export default Bridge;
+export default bridge;
